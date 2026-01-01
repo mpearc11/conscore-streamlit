6c8c9030f38170a242b8b0ca61fc66e51d4a6d03
mpearc11 <mpearc11@jhu.edu>
2026-01-01 15:55:49 -0500

    Update conscore-streamlit.py

--- conscore-streamlit.py
+++ conscore-streamlit.py
@@ -51,7 +51,7 @@ if st.button('make msa df & freq df'):
 
     #make frequency df
     freq_df = pd.DataFrame(columns = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y'])
-    for idx,i in enumerate(msa_df['0']):
+    for idx,i in enumerate(msa_df.iloc[:,idx]):
         st.write(idx)
         st.write(i)
     
