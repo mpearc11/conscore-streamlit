ca924411b835d271d4aa24d771972df36b771ef7
mpearc11 <mpearc11@jhu.edu>
2026-01-02 12:32:23 -0500

    Update conscore-streamlit.py

--- conscore-streamlit.py
+++ conscore-streamlit.py
@@ -56,12 +56,12 @@ if st.button('make msa df & freq df'):
     for i in range(len(msa_df.columns)):
         #st.write(i)
         #st.write(msa_df.iloc[0,i])
-        #for a in range(len(aa)):
-        st.write(msa_df.iloc[:,0])
-        freqaa = (msa_df.iloc[:,0] == 'M').sum()
-        st.write(freqaa)
-        #freq_df.iloc[0,0] = freqaa
-        #st.write(freq_df)
+        for a in range(len(aa)):
+            #st.write(msa_df.iloc[:,0])
+            freqaa = (msa_df.iloc[:,0] == aa(a)).sum()
+            #st.write(freqaa)
+            freq_df.iloc[i,a] = freqaa
+            st.write(freq_df)
         
     
 
