f61f815b4efbaf4c7b2144489a3ccf8882dce4b6
mpearc11 <mpearc11@jhu.edu>
2026-01-02 21:23:26 -0500

    Update conscore-streamlit.py

--- conscore-streamlit.py
+++ conscore-streamlit.py
@@ -21,7 +21,7 @@ st.header('Submit MSA')
     
 ##below is core of code for clustal and consurf uploads & alignment in df
 
-msa_file = st.file_uploader("",type='fa')
+msa_file = st.file_uploader("",type='fasta')
 if msa_file is not None:
     st.success("MSA file uploaded")
 else:
